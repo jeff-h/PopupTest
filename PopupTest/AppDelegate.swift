d9ee5cff0b43f7cf4e472df344fe70867aacbffc
Jeff Hanbury <jeff@marmaladesoul.com>
2014-08-16 17:45:57 +1200

    Initial Commit

---
+++ PopupTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PopupTest
+//
+//  Created by Jeff Hanbury on 16/08/2014.
+//  Copyright (c) 2014 Marmaladesoul. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PopupTest/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  PopupTest
+//
+//  Created by Jeff Hanbury on 16/08/2014.
+//  Copyright (c) 2014 Marmaladesoul. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
